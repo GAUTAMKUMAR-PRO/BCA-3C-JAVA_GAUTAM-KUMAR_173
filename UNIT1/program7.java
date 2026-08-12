35bdf7f302c08d39ec76531b6870bc3bacbaf2c5
Gautam Kumar <roy12ff1998@gmail.com>
2026-08-12 22:43:21 +0530

    Add voting eligibility check in program7.java  if condition

---
+++ UNIT1/program7.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class pro7
+{
+ public static void main(String[]args)
+ {
+    Scanner r =new Scanner(System.in);
+
+int age=20;
+if(age>=18)
+{
+System.out.println("eligible for voting:");
+}
+
+  
+  System.out.println("thank you");
+
+       }
+
+    }

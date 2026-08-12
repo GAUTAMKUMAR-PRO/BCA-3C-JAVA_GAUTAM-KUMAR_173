c8b804a04b9bd6da07d56c763152e90c3224213c
Gautam Kumar <roy12ff1998@gmail.com>
2026-08-12 23:38:08 +0530

    Add loop to print numbers from 0 to 5

---
+++ UNIT1/program6_1.java
@@ -0,0 +1,18 @@
+//write print for loop 5 time//
+import java.util.Scanner;
+
+	class pro61{
+	
+		public static void main(String[] args){
+			Scanner sc= new Scanner(System.in);
+
+		int i;
+		for(i=0;i<=5;i++)
+         {
+		System.out.println(i);
+		}
+		
+		
+		}
+
+	}

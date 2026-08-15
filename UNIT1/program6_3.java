8c4ad385819c044ef142a995a7a8fbd5fb09b487
Gautam Kumar <roy12ff1998@gmail.com>
2026-08-15 15:09:23 +0530

    Add Java program with while loop example
    Implement a Java program using a while loop to print numbers from 0 to 4.

---
+++ UNIT1/program6_3.java
@@ -0,0 +1,14 @@
+//write a java program for while loop.
+public class pro63 {
+
+    public static void main(String[]args){
+        int i=0;
+        while(i<5)
+        {
+            System.out.println(i);
+            i++;
+        }
+
+          }
+
+    }        

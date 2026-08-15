f93b03e6c3243d023632c0d38e2df1047c6d8261
Gautam Kumar <roy12ff1998@gmail.com>
2026-08-15 22:46:08 +0530

    Add pro69 class to print name multiple times

---
+++ UNIT1/program6_9.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class pro69 {
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("enter the name:");
+        String name=sc.nextLine();
+
+        System.out.println("enter the number:");
+        int n=sc.nextInt();
+
+        int i=0;
+
+
+        do{
+
+        System.out.println(i+name);
+        i++;
+        }
+
+        while(i<=n);
+    
+        
+            
+
+            }
+        }
+        
+
+
+    

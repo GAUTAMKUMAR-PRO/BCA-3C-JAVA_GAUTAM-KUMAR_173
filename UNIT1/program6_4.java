f06d245cb947b0d01b7baa268e8d154345ff12dd
Gautam Kumar <roy12ff1998@gmail.com>
2026-08-15 15:32:41 +0530

    Add program to print numbers using for loop
    Implement a Java program to print numbers from 0 to the input number using a for loop.

---
+++ UNIT1/program6_4.java
@@ -0,0 +1,18 @@
+//write a java program for number of run using for loop.
+import java.util.Scanner;
+
+public class pro64 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+   System.out.println("enter the numbr:");
+   int g =sc.nextInt();
+
+   int i;
+   for(i=0;i<=g;i++){
+    System.out.println(i);
+   }
+   
+    }
+
+}

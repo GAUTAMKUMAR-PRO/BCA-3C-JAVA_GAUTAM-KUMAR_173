32603803959993159833b1ddf17a5f87e840a1b9
Gautam Kumar <roy12ff1998@gmail.com>
2026-08-29 16:46:58 +0530

    Add program9_7.java for reverse row pattern
    Implemented a Java program to print a reverse row pattern.

---
+++ UNIT1/program9_7.java
@@ -0,0 +1,22 @@
+//write a Java Program to Print Reverse Row Pattern?
+public class program9_7 {
+    
+    
+    public static void main(String[]args){
+
+    
+
+        for(int row=5;row>=1;row--){
+
+            for(int cols=1;cols<=row;cols++){
+                System.out.print("row");
+                
+            }
+
+            System.out.println();
+            
+        }
+    }
+    
+}
+    

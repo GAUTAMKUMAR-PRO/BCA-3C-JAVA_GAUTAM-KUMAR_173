734904b4ea3898c73508fcfa301192908a3e07e9
Gautam Kumar <roy12ff1998@gmail.com>
2026-08-29 16:20:30 +0530

    Create program9_4.java

---
+++ UNIT1/program9_4.java
@@ -0,0 +1,18 @@
+// write a Java Program to Print Pattern 4 (Mirror Pattern)?
+public class program9_4 {
+    public static void main(String[]args){
+
+    
+
+        for(int row=1;row<=5;row++){
+
+            for(int cols=1;cols<=row;cols++){
+                System.out.print("louts");
+            }
+
+            System.out.println();
+            
+        }
+    }
+    
+}

14427b30a0b05064a10c05941bd0dde3e9a06c2a
Gautam Kumar <roy12ff1998@gmail.com>
2026-09-02 17:46:53 +0530

    Add program to demonstrate final keyword in Java
    Implemented a Java program demonstrating the use of the final keyword.

---
+++ UNIT2/programo_13.java
@@ -0,0 +1,11 @@
+// write a java program of final keyword?
+public class program13 {
+    public static void main(String[]args){
+    
+        final int rollNO=101;
+        final double CGPA=8.5;
+            System.out.println("Roll Number: " + rollNO);
+            System.out.println("CGPA: " + CGPA);
+        }
+
+    }

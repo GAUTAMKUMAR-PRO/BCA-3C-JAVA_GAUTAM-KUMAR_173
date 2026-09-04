8626aa8e987de96d597e62fb0ec4bfe9fc87e736
Gautam Kumar <roy12ff1998@gmail.com>
2026-09-04 11:48:02 +0530

    Create programo_15.java

---
+++ UNIT2/programo_15.java
@@ -0,0 +1,19 @@
+// write a java progam of final class?
+final class vivaansecureinformation {
+
+     void showdata(){
+
+        System.out.println("secure information verified boimatrics");
+    }
+    
+}
+
+class program15{
+    public static void main(String[]args){
+        vivaansecureinformation obj=new vivaansecureinformation();
+         obj.showdata();
+         obj.showdata();
+
+
+    }
+}
